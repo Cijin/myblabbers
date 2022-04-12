c5b73423bea18a14540d0e3c8733ac46c4680b47
cijin <cijininurhood@gmail.com>
2022-04-12 08:55:13 +0700

    Hardcode username temporarily

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // @TODO: Remove this once react-query updated
   typescript: {
     ignoreBuildErrors: true,
   },
